22ee8ea40d0e21d8c74f935252ae4a55f9f87714
King Sovereign <87546781+kingsovereign@users.noreply.github.com>
2024-01-16 22:39:49 -0900

    update name of script title bar

--- config-profiles/kasm-singleserver-init.sh
+++ config-profiles/kasm-singleserver-init.sh
@@ -2,7 +2,7 @@
 
 #/!*!*---->This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.<----*!*!\
 
-################### LANd DOCKGE INSTALL SCRIPT ####################################
+################### LANd KASM SINGLE SERVER INSTALL SCRIPT ####################################
 
 ##################SCRIPT CONFIG################
 #Set timezone to "America/Anchorage"
