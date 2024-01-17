90ab9976fc1b9ceae9d8675cb4172b7d562c75be
King Sovereign <87546781+kingsovereign@users.noreply.github.com>
2024-01-16 22:34:18 -0900

    move MLP disclaimer to above lead title bar

--- config-profiles/kasm-singleserver-init.sh
+++ config-profiles/kasm-singleserver-init.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-################### LANd DOCKGE INSTALL SCRIPT ####################################
-#!!---->This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.<----!!
+#/!*!*---->This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.<----*!*!\
 
-######
+################### LANd DOCKGE INSTALL SCRIPT ####################################
 
 ##################SCRIPT CONFIG################
 #Set timezone to "America/Anchorage"
