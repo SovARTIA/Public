b43351000574611e7d0c40c4b8860f536150b579
King Sovereign <87546781+kingsovereign@users.noreply.github.com>
2024-01-16 20:10:49 -0900

    add MPL to applicable files

--- config-profiles/kasm-singleserver-init.sh
+++ config-profiles/kasm-singleserver-init.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+################### LANd DOCKGE INSTALL SCRIPT ####################################
+#!!---->This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.<----!!
+
+######
+
 ##################SCRIPT CONFIG################
 #Set timezone to "America/Anchorage"
 timedatectl set-timezone America/Anchorage
