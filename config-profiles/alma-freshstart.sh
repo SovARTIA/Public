3d1bf8fc50d9bae609034bb5c4d7a58ef863e92e
King Sovereign <87546781+kingsovereign@users.noreply.github.com>
2024-01-17 23:13:18 -0900

    update alma script

--- config-profiles/alma-freshstart.sh
+++ config-profiles/alma-freshstart.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env bash
 
-set -x
+#/!*!*---->This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. © 2024 | Sovereignty ARTIA. If a copy of the MPL was not distributed with this file, You can obtain one at https://github.com/SovARTIA/Public/blob/a2d5042a7eb7b3e5302b51356f9e4d1fcec14459/LICENSES/MPL.md.<----*!*!\
 
-######### THIS SCRIPT WILL UPDATE AND UPGRADE THE SYSTEM THEN INSTALL DOCKER + PORTAINER AND RESTART THE SYSTEM #####
+#Set timezone to "America/Anchorage"
+timedatectl set-timezone America/Anchorage #set to current timezone
+
+# Enable error handling
+set -e
+
+# Redirect standard error to a file
+exec 2> error.log
+
+# Function to handle errors
+handle_error() {
+    echo "Error on line $1: '$2'" >> error.log
+}
+
+# Trap errors with the error handler
+trap 'handle_error $LINENO "$BASH_COMMAND"' ERR
+
+######### THIS SCRIPT WILL UPDATE AND UPGRADE THE SYSTEM THEN RESTART THE SYSTEM #####
 
 echo \
 "fastestmirror=True
@@ -16,10 +33,12 @@ dnf clean all
 
 dnf autoremove -y
 
-#### Enable Cockpit 
+#### Enable Cockpit | more information on cockpit -----------> 
 
 systemctl enable --now cockpit.socket
 
 systemctl start cockpit
 
 systemctl reboot
+
+#/#/EOF\#\#
--- config-profiles/kasm-singleserver-init.sh
+++ config-profiles/kasm-singleserver-init.sh
@@ -6,7 +6,7 @@
 
 ##################SCRIPT CONFIG################
 #Set timezone to "America/Anchorage"
-timedatectl set-timezone America/Anchorage
+timedatectl set-timezone America/Anchorage #set to current timezone
 
 # Enable error handling
 set -e
@@ -22,13 +22,6 @@ handle_error() {
 # Trap errors with the error handler
 trap 'handle_error $LINENO "$BASH_COMMAND"' ERR
 
-# Define a function that prints a message when the script exits
-function on_exit {
-  echo "AN ERROR HAS OCCURED DURING THE UPDATE AND INSTALL PROCESS OF KASM, PLEASE CHECK THE \"error.log\" file for details."
-}
-
-# Set a trap to call the function when the script exits
-trap on_exit EXIT
 ##############################################
 #This phase will install the Volian Repos for Nala install
 echo "deb http://deb.volian.org/volian/ scar main" > /etc/apt/sources.list.d/volian-archive-scar-unstable.list
